3aae10a04b02c84ea93cf785bfc9ea035472b7b2
Gabrielivensdds <gabrielivensdds@gmail.com>
2022-06-14 17:50:17 -0300

    Upload 1

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from './botao.module.scss'
+
+class Button extends React.Component<{
+    children?: React.ReactNode,
+    type?: "button" | "submit" | "reset" | undefined
+}> {
+
+
+    render() {
+        const { type = "button" } = this.props;
+        return (
+            <button type={type} className={style.botao}>
+                {this.props.children}
+            </button>
+        )
+    }
+}
+
+export default Button;
